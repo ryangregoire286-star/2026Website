9c5601715f7b31035f1d271274a1718f8efde5c2
ryangregoire286-star <ryangregoire286@gmail.com>
2026-05-01 13:52:23 -0400

    added route function

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,8 @@ use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
 
-    let listen_server =
-        tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-
+    let listen_server = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listen_server, start_router()).await.unwrap();
 }
 
@@ -17,10 +13,20 @@ async fn hello() -> &'static str {
     "Hello I am Ryan James"
 }
 
+fn get_route(route: String) -> String {
+    if route.contains("/") {
+            "/";
+    } else if route.contains("/About") {
+        "/About";
+    } else if route.contains("/Contact") {
+        "/Contact";
+    }
+
+    format!("{:?}", "/")
+}
+
 fn start_router() -> Router {
-
     let cors = CorsLayer::new().allow_origin(Any);
-
-    let ser = Router::new().route("/", get(hello)).layer(cors);
+    let ser = Router::new().route(&get_route("/".to_string()), get(hello)).layer(cors);
     ser
 }
